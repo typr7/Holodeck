c9dfa0cb285e88eb673a30af10ee618510ff5c73
typr7 <blame773@gmail.com>
2025-07-02 15:03:32 +0800

    generate dataset

--- tools/generate_dataset.py
+++ tools/generate_dataset.py
@@ -1,5 +1,7 @@
 import os
 import uuid
+import json
+import gzip
 import shutil
 import inspect
 import traceback
@@ -60,34 +62,36 @@ def create_episode(
         "object_category": object_category
     }
 
-_hm3d_categories = (
-    "chair",
-    "bed",
-    "plant",
-    "toilet",
-    "tv_monitor",
-    "sofa"
-)
-
-def create_object_info_list_from_scene(scene_json) -> List:
-    object_info_list = list()
+def create_object_info_list(scene_json, scene_uuid) -> Dict[str, List]:
+    hm3d_by_category = {
+        "chair": [],
+        "bed": [],
+        "plant": [],
+        "toilet": [],
+        "tv_monitor": [],
+        "sofa": []
+    }
 
     invert_x = lambda pos: [-pos[0], pos[1], pos[2]]
 
     for i, obj in enumerate(scene_json['objects']):
         obj_id = obj['id']
         obj_cls = obj_id.split('-')[0]
-        if obj_cls in _hm3d_categories:
+        if obj_cls in hm3d_by_category.keys():
             # holodeck 生成的模型由 unity 插件 gltfast 导出，unity 使用左手坐标系，gltf 模型使用右手坐标系，gltfast 在导出时会将 x 坐标取反
             object_position = invert_x(obj['position'])
             object_name = f'{obj_cls}_{i}'
             object_info = create_object_info(object_position, i, object_name, obj_cls)
 
-            object_info_list.append(object_info)
+            hm3d_by_category[obj_cls].append(object_info)
         
-    return object_info_list
+    ret = dict()
+    for cls, obj_info_list in hm3d_by_category.items():
+        ret[f'{scene_uuid}.basis.glb_{cls}'] = obj_info_list
+    
+    return ret
 
-def create_episode_list_from_scene(scene_json) -> List:
+def create_episode_list(scene_json, scene_id, scene_dataset_config_path) -> List:
     pass
 
 def generate_glb_scene(scene_json, scene_path):
@@ -131,34 +135,64 @@ def generate_scene_navmesh(scene_path, save_path):
         raise Exception(f'failed to save scene navmesh')
     sim.close()
 
-def create_training_data(scene_json):
+def generate_training_json(scene_json, training_json_path, scene_uuid, scene_id, scene_dataset_config_path):
+    goals_by_category = create_object_info_list(scene_json)
+    episode_list = create_episode_list(scene_json, scene_id, scene_dataset_config_path)
+
+    training_json = {
+        "goals_by_category": goals_by_category,
+        "episodes": episode_list,
+        "category_to_task_category_id": {
+            "chair": 0,
+            "bed": 1,
+            "plant": 2,
+            "toilet": 3,
+            "tv_monitor": 4,
+            "sofa": 5
+        },
+        "category_to_scene_annotation_category_id": {
+            "chair": 0,
+            "bed": 1,
+            "plant": 2,
+            "toilet": 3,
+            "tv_monitor": 4,
+            "sofa": 5
+        }
+    }
+
+    with gzip.open(os.path.join(training_json_path, scene_uuid + '.json.gz'), 'w') as fp:
+        json.dump(training_json, fp)
+
+def generate_single_training_data(scene_json):
+    training_data_base_path = os.environ["TRAINING_DATA_BASEPATH"]
+
     scene_uuid = uuid.uuid4().hex
 
-    training_data_base_path = os.environ["TRAINING_DATA_BASEPATH"]
+    scene_id = os.path.join('hm3df', 'train', scene_uuid, scene_uuid + '.basis.glb')
+
     scene_dataset_path = os.path.join(training_data_base_path,
                                       'scene_datasets',
                                       'hm3df')
-    scene_dataset_config_path = os.path.join(scene_dataset_path,
-                                             'hm3df_annotated_basis.scene_dataset_config.json')
     scene_dir_path = os.path.join(scene_dataset_path,
                                   'train',
                                   scene_uuid)
-    scene_path = os.path.join(scene_dir_path,
-                              scene_uuid + '.basis.glb')
-    scene_navmesh_path = os.path.join(scene_dir_path,
-                                      scene_uuid + '.basis.navmesh')
+    scene_path = os.path.join(scene_dir_path, scene_uuid + '.basis.glb')
+    scene_navmesh_path = scene_path.removesuffix('.glb') + '.navmesh'
+    training_json_path = os.path.join(training_data_base_path,
+                                      'datasets', 'hm3df', 'train', 'content')
+
+    scene_dataset_config_path = os.path.join('./data', 'scene_datasets', 'hm3df',
+                                             'hm3df_annotated_basis.scene_dataset_config.json')
     
     os.makedirs(scene_dir_path, exist_ok=True)
 
     try:
         generate_glb_scene(scene_json, scene_path)
+
         generate_scene_navmesh(scene_path, scene_navmesh_path)
 
-        # object info
-        object_info_list = create_object_info_list_from_scene(scene_json)
+        generate_training_json(scene_json, training_json_path, scene_id, scene_dataset_config_path)
 
-        # episodes
-        episode_list = create_episode_list_from_scene(scene_json)
     except Exception as e:
         print(f'{Fore.RED}{__file__}: '
               f'{inspect.currentframe().f_code.co_name}: '
@@ -166,3 +200,11 @@ def create_training_data(scene_json):
         traceback.print_exc()
 
         shutil.rmtree(scene_dir_path)
+
+if __name__ == "__main__":
+    scene_json_path = "/home/wu/Documents/Holodeck/data/scenes/Apartment-2025-06-04-15-42-53-471178/Apartment.json"
+
+    with open(scene_json_path, 'r') as fp:
+        scene_json = json.load(fp)
+
+    generate_glb_scene(scene_json)
